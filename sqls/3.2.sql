f27678c65291b835b6eaa00195fe734b6e1f9c38
Yosuke Mizuguchi <mizuguchi.yosuke.fcp@gmail.com>
2021-04-11 14:48:17 +0900

    Fix: create docker-compose.yml

---
+++ sqls/3.2.sql
@@ -0,0 +1,6 @@
+insert into testtable1(id, name, age)
+values (101, 'Alice', 20);
+insert into testtable1(id, name, age)
+values (102, 'Bob', 25);
+insert into testtable1(id, name, age)
+values (103, 'Cathy', 22);
